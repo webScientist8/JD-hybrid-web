91403b1213a2fb4fc6bdea84f8c6024c025d61b6
webScientist8 <l>
2019-08-26 11:14:03 +0800

    5-10

--- src/components/goods/Goods.vue
+++ src/components/goods/Goods.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="goods goods-waterfall">
-      <div class="goods-item goods-waterfall-item" v-for="(item, index) in dataSource" :key="index">
-        <img class="goods-item-img" :src="item.img" alt="">
+    <div class="goods goods-waterfall" :style="{height: goodsViewHeight}">
+      <div class="goods-item goods-waterfall-item" ref="goodsItem" v-for="(item, index) in dataSource" :key="index" :style="goodsItemStyles[index]">
+        <img class="goods-item-img" :src="item.img" alt="" :style="imgStyles[index]">
         <div class="goods-item-desc">
           <p class="goods-item-desc-name">
             <span class="text-line-2">{{item.name}}</span>
@@ -20,7 +20,13 @@ export default {
   name: 'Goods',
   data: function () {
     return {
-      dataSource: []
+      dataSource: [],
+      MIN_IMG_HEIGHT: 178,
+      MAX_IMG_HEIGHT: 230,
+      imgStyles: [],
+      ITEM_MARGIN_SIZE: 8,
+      goodsItemStyles: [],
+      goodsViewHeight: 0
     }
   },
   created: function () {
@@ -31,7 +37,49 @@ export default {
       this.$http.get('/goods')
         .then(data => {
           this.dataSource = data.list
+          this.initImgStyles()
+          this.$nextTick(() => {
+            this.initWaterfall()
+          })
         })
+    },
+    imgHeight: function () {
+      let result = Math.floor(Math.random() * (this.MAX_IMG_HEIGHT - this.MIN_IMG_HEIGHT) + this.MIN_IMG_HEIGHT)
+      return result
+    },
+    initImgStyles: function () {
+      this.dataSource.forEach(item => {
+        let imgHeight = this.imgHeight() + 'px'
+        this.imgStyles.push({
+          height: imgHeight
+        })
+      })
+    },
+    initWaterfall: function () {
+      let $goodsItems = this.$refs.goodsItem
+      if (!$goodsItems) return
+      let leftHeightTotal = 0
+      let rightHeightTotal = 0
+      $goodsItems.forEach(($el, index) => {
+        let goodsItemStyle = {}
+        let elHeight = $el.clientHeight + this.ITEM_MARGIN_SIZE
+
+        if (leftHeightTotal <= rightHeightTotal) {
+          goodsItemStyle = {
+            left: '0px',
+            top: leftHeightTotal + 'px'
+          }
+          leftHeightTotal += elHeight
+        } else {
+          goodsItemStyle = {
+            right: '0px',
+            top: rightHeightTotal + 'px'
+          }
+          rightHeightTotal += elHeight
+        }
+        this.goodsItemStyles.push(goodsItemStyle)
+      })
+      this.goodsViewHeight = (leftHeightTotal > rightHeightTotal ? leftHeightTotal : rightHeightTotal) + 'px'
     }
   }
 }
